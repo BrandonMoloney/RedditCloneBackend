2906dd48b6fd4d0a39453564841ffd742151a8c8
BrandonMoloney <brandonsmoloney@gmail.com>
2022-09-25 13:39:12 -0400

    adding cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000  
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.status(200).send('hello world')
 })
